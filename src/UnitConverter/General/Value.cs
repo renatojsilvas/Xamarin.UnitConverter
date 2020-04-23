8603a00be54572f3da2679d809499b0cc12d7456
Renato Silva <renatojsilvas@gmail.com>
2020-04-22 22:41:46 -0300

    Value added

---
+++ src/UnitConverter/General/Value.cs
@@ -0,0 +1,37 @@
+﻿using Math.Rounding;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitConverter
+{
+    public class Value
+    {
+        public Value(double amount, Unit unit)
+        {
+            Amount = amount;
+            Unit = unit;
+        }
+
+        public Value(double amount) 
+            : this(amount, string.Empty)
+        {
+        }
+        
+        public Value(double amount, string symbol)
+        {
+            Amount = amount;
+            Unit = DataRepository.GetUnitByFullName(symbol);
+            if (Unit != null) return;
+            Unit = DataRepository.GetUnitBySymbol(symbol);
+        }
+
+        public double Amount { get; }
+        public Unit Unit { get; }
+
+        public string ToString(int significantFigures)
+        {
+            return $"{Amount.ToString(significantFigures)} {Unit.Symbol}".TrimEnd();
+        }
+    }
+}
---
+++ tests/UnitConverter.Tests/ValueTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using System.Globalization;
+
+namespace UnitConverter.Tests
+{
+    public class ValueTests
+    {
+        [Fact]
+        public void Create_value_from_unit_symbol()
+        {
+            //Arrange
+            Value sut = new Value(1.25, "°C");
+
+            //Act
+
+
+            //Assert
+            sut.Unit.Should().BeAssignableTo<Celsius>();
+            sut.Amount.Should().Be(1.25);
+            sut.ToString(2).Replace(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator, ".").Should().Be("1.2 °C");
+        }
+
+        [Fact]
+        public void Create_value_from_unit_full_name()
+        {
+            //Arrange
+            Value sut = new Value(1.25, "Celsius");
+
+            //Act
+
+
+            //Assert
+            sut.Unit.Should().BeAssignableTo<Celsius>();
+            sut.Amount.Should().Be(1.25);
+            sut.ToString(2).Replace(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator, ".").Should().Be("1.2 °C");
+        }
+
+        [Fact]
+        public void Create_value_from_unit()
+        {
+            //Arrange
+            Value sut = new Value(1.25, new Celsius());
+
+            //Act
+
+
+            //Assert
+            sut.Unit.Should().BeAssignableTo<Celsius>();
+            sut.Amount.Should().Be(1.25);
+            sut.ToString(2).Replace(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator, ".").Should().Be("1.2 °C");
+        }
+    }
+}
