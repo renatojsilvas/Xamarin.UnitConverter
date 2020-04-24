4a392634bc2f350f7e61f12664a6c6efc5095e02
Renato Silva <renatojsilvas@gmail.com>
2020-04-23 21:34:31 -0300

    UnitConverterService added

---
+++ src/UnitConverter/Services/UnitConverterService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter
+{
+    public class UnitConverterService
+    {
+        public Task<ServiceResult> ConvertUnit(double value, string source, string destination)
+        {
+            Unit sourceUnit = SelectUnit(source);
+            Unit destinationUnit = SelectUnit(destination);
+            UnitsConverter unitsConverter = new UnitsConverter(sourceUnit, destinationUnit);
+            double convertedValue = unitsConverter.Convert(value);
+            ServiceResult result = new ServiceResult(true, string.Empty, new Value(convertedValue, destinationUnit.Symbol));
+            return Task.FromResult(result);
+        }
+
+        private Unit SelectUnit(string unit)
+        {
+            Unit sourceUnit = DataRepository.GetUnitByFullName(unit);
+            if (sourceUnit == null) 
+                sourceUnit = DataRepository.GetUnitBySymbol(unit);
+            return sourceUnit;
+        }
+    }
+}
---
+++ tests/UnitConverter.Tests/ServicesTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+
+namespace UnitConverter.Tests
+{
+    public class ServicesTests
+    {
+        [Fact]
+        public async void Convert_units_using_full_name_as_reference()
+        {
+            //Arrange
+            UnitConverterService sut = new UnitConverterService();
+
+            //Act
+            ServiceResult result = await sut.ConvertUnit(0, "Celsius", "Fahrenheit");
+
+            //Assert
+            result.Success.Should().BeTrue();
+            result.Message.Should().Be(String.Empty);
+            result.Value.ToString(2).Should().Be("32 °F");
+        }
+
+        [Fact]
+        public async void Convert_units_using_symbol_as_reference()
+        {
+            //Arrange
+            UnitConverterService sut = new UnitConverterService();
+
+            //Act
+            ServiceResult result = await sut.ConvertUnit(0, "°C", "°F");
+
+            //Assert
+            result.Success.Should().BeTrue();
+            result.Message.Should().Be(String.Empty);
+            result.Value.ToString(2).Should().Be("32 °F");
+        }
+    }
+}
