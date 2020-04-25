d33442c96c70b662991e127b8b6934a80501488b
Renato Silva <renatojsilvas@gmail.com>
2020-04-25 17:21:42 -0300

    LoadQuantitiesService added

---
+++ src/UnitConverter/Services/LoadQuantitiesService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter
+{
+    public class LoadQuantitiesService
+    {
+        public Task<(bool Success, string Message, List<string> Quantities)> LoadQuantities()
+        {
+            return Task.FromResult((true, string.Empty, DataRepository.GetQuantities()));
+        }
+    }
+}
---
+++ tests/UnitConverter.Tests/LoadQuantitiesServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace UnitConverter.Tests
+{
+  
+    public class LoadQuantitiesServiceTests
+    {
+        [Fact]
+        public async void Load_quantities()
+        {
+            //Arrange
+            LoadQuantitiesService sut = new LoadQuantitiesService();
+
+            //Act
+            var result = await sut.LoadQuantities();
+
+            //Assert
+            result.Success.Should().BeTrue();
+            result.Message.Should().Be(string.Empty);
+            result.Quantities.Should().HaveCount(2);
+            result.Quantities.Should().Contain("Temperature");
+            result.Quantities.Should().Contain("Pressure");
+        }
+    }
+}
