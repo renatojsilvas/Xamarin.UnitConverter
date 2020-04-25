845ee92cc26f8d5809b550c0ef9ff294a265c05a
Renato Silva <renatojsilvas@gmail.com>
2020-04-25 17:30:38 -0300

    LoadUnitsService added

---
+++ src/UnitConverter/Services/LoadUnitsService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter
+{
+    public class LoadUnitsService
+    {
+        public Task<(bool Success, string Message, List<string> Units)> LoadUnits(string quantity)
+        {
+            return Task.FromResult((true, string.Empty, DataRepository.GetUnitsByQuantity(quantity).Select(u => u.FullName).ToList()));
+        }
+    }
+}
---
+++ tests/UnitConverter.Tests/LoadUnitsServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+
+namespace UnitConverter.Tests
+{
+    public class LoadUnitsServiceTest
+    {
+        [Fact]
+        public async void Load_units_by_quantity()
+        {
+            //Arrange
+            LoadUnitsService sut = new LoadUnitsService();
+
+            //Act
+            var result = await sut.LoadUnits("Temperature");
+
+            //Assert
+            result.Success.Should().BeTrue();
+            result.Message.Should().Be(string.Empty);
+            result.Units.Should().HaveCount(4);
+            result.Units.Should().AllBeAssignableTo<string>();
+            result.Units.Should().Contain("Celsius");
+            result.Units.Should().Contain("Fahrenheit");
+            result.Units.Should().Contain("Kelvin");
+            result.Units.Should().Contain("Rankine");
+        }
+    }
+}
