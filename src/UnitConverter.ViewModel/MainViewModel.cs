09fe98f48501bd647b5240b504c61fa7064dca41
Renato Silva <renatojsilvas@gmail.com>
2020-04-26 10:20:55 -0300

    Added ViewModel project and the initial test

---
+++ src/UnitConverter.ViewModel/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace UnitConverter.ViewModel
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            Quantities = new ObservableCollection<string>();
+            Quantities.Add("Temperature");
+            Quantities.Add("Pressure");
+            SelectedQuantity = "Temperature";
+            SourceUnits = new ObservableCollection<string>();
+            SourceUnits.Add("Celsius");
+            SourceUnits.Add("Fahrenheit");
+            SourceUnits.Add("Kelvin");
+            SourceUnits.Add("Rankine");
+            DestinationUnits = new ObservableCollection<string>();
+            DestinationUnits.Add("Celsius");
+            DestinationUnits.Add("Fahrenheit");
+            DestinationUnits.Add("Kelvin");
+            DestinationUnits.Add("Rankine");
+            SourceValue = "0 °C";
+            DestinationValue = "0 °C";
+        }
+
+        public ObservableCollection<string> Quantities { set; get; }
+        public string SelectedQuantity { set; get; }
+        public ObservableCollection<string> SourceUnits { set; get; }
+        public ObservableCollection<string> DestinationUnits { set; get; }
+        public string SourceValue { set; get; }
+        public string DestinationValue { set; get; }
+    }
+}
---
+++ tests/UnitConverter.Tests/MainViewModelTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using UnitConverter.ViewModel;
+
+namespace UnitConverter.Tests
+{
+    public class MainViewModelTests
+    {
+        [Fact]
+        public void Ìnitial_state_of_view()
+        {
+            //Arrange
+            MainViewModel sut = new MainViewModel();
+
+            //Act
+
+            //Assert
+            sut.Quantities.Should().HaveCount(2);
+            sut.Quantities.Should().ContainInOrder("Temperature", "Pressure");
+            sut.SelectedQuantity.Should().Be("Temperature");
+            sut.SourceUnits.Should().HaveCount(4);
+            sut.SourceUnits.Should().ContainInOrder("Celsius", "Fahrenheit", "Kelvin", "Rankine");
+            sut.DestinationUnits.Should().HaveCount(4);
+            sut.DestinationUnits.Should().ContainInOrder("Celsius", "Fahrenheit", "Kelvin", "Rankine");
+            sut.SourceValue.Should().Be("0 °C");
+            sut.DestinationValue.Should().Be("0 °C");
+        }
+    }
+}
