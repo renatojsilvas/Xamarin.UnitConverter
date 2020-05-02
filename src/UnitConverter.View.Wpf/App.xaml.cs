8ca50951f0b45a2497a336bbfdd07e76ad78e30e
Renato Silva <renatojsilvas@gmail.com>
2020-05-02 16:23:13 -0300

    Refactorations

--- src/UnitConverter.View.Wpf/App.xaml.cs
+++ src/UnitConverter.View.Wpf/App.xaml.cs
@@ -16,27 +16,10 @@ namespace UnitConverter.View.Wpf
     {
         public App()
         {
-            SetLanguageDictionary();
             MainWindow window = new MainWindow();
             window.Show();
         }
 
-        private void SetLanguageDictionary()
-        {
-            ResourceDictionary dict = new ResourceDictionary();
-            switch (Thread.CurrentThread.CurrentCulture.ToString())
-            {
-                case "en-US":
-                    dict.Source = new Uri("..\\Resources\\StringResources.xaml", UriKind.Relative);
-                    break;
-                case "fr-CA":
-                    dict.Source = new Uri("..\\Resources\\StringResources.pt-BR.xaml", UriKind.Relative);
-                    break;
-                default:
-                    dict.Source = new Uri("..\\Resources\\StringResources.xaml", UriKind.Relative);
-                    break;
-            }
-            this.Resources.MergedDictionaries.Add(dict);
-        }
+       
     }
 }
