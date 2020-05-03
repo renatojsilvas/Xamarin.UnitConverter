b5cd6cf1ea5e51cc70549790803493a68ae8311f
Renato Silva <renatojsilvas@gmail.com>
2020-05-03 15:01:00 -0300

    Initial Version for Android

---
+++ src/UnitConverter.View.Xamarin/UnitConverter.View.Xamarin/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnitConverter.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UnitConverter.View.Xamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            ILoadQuantitiesService loadQuantitiesService = new LoadQuantitiesService();
+            ILoadUnitsService loadUnitsService = new LoadUnitsService();
+            IUnitConverterService unitConverterService = new UnitConverterService();
+            MainViewModel mainViewModel = new MainViewModel(loadQuantitiesService, loadUnitsService, unitConverterService);
+            MainPage = new MainPage()
+            {
+                MainViewModel = mainViewModel
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ src/UnitConverter.View.Xamarin/UnitConverter.View.Xamarin/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitConverter.ViewModel;
+using Xamarin.Forms;
+
+namespace UnitConverter.View.Xamarin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public MainViewModel MainViewModel
+        {
+            set
+            {
+                this.BindingContext = value;
+            }
+        }
+    }
+}
